9f0900164c7ca2e3bbaa82c1c4524743f278b7cb
tikiardo <thiagogroba2@gmail.com>
2023-05-24 12:41:46 +0100

    anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  const[points,setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 })
+  const[points,setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
   const [mostVotedAnecdote, setMostVotedAnecdote] = useState(0)
   
   const vote = () => {
@@ -41,8 +41,8 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  'Ser Maricon es debil.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  
 ]
 
 
